88493f1e8901845e52a95a1002d25c61d1681472
Tengu712 <genreihoutengu@gmail.com>
2024-04-13 14:33:29 +0900

    solve

--- atcoder/abc/346/b.rs
+++ atcoder/abc/346/b.rs
@@ -40,4 +40,55 @@ where
         .collect::<Vec<T>>()
 }
 
-fn main() {}
+fn get_min(b: u64) -> u64 {
+    if b < 2 {
+        return 0;
+    }
+    let mut res = 0;
+    let mut a = 4;
+    let mut mode = true;
+    for i in 2..(b + 1) {
+        res += 1;
+        if i >= a {
+            res += 1;
+            a += if mode { 2 } else { 3 };
+            mode = !mode;
+        }
+    }
+    res
+}
+
+fn get_max(b: u64) -> u64 {
+    let mut res = 2;
+    let mut a = 2;
+    let mut mode = true;
+    for i in 1..(b + 1) {
+        res += 1;
+        if i >= a {
+            res += 1;
+            a += if mode { 3 } else { 2 };
+            mode = !mode;
+        }
+    }
+    res
+}
+
+fn main() {
+    let wb = get_ns::<u64>();
+    let w = wb[0];
+    let b = wb[1];
+
+    if w < b {
+        if b - w < 2 {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    } else {
+        if get_min(b) <= w && w <= get_max(b) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
+}
--- atcoder/abc/348/b.rs
+++ atcoder/abc/348/b.rs
@@ -1,18 +1,34 @@
-#![allow(dead_code, unused_imports)]
+#![allow(dead_code)]
 
-use std::collections::*;
+fn main() {
+    let n = get_n::<u64>();
+
+    let mut ps = Vec::new();
+    for _ in 0..n {
+        let xy = get_ns::<i64>();
+        ps.push((xy[0], xy[1]));
+    }
+
+    for i in 0..ps.len() {
+        let mut max = 0;
+        let mut max_idx = 0;
+        for j in 0..ps.len() {
+            let p1 = ps[i];
+            let p2 = ps[j];
+            let dis = (p1.0 - p2.0).pow(2) + (p1.1 - p2.1).pow(2);
+            if max < dis {
+                max = dis;
+                max_idx = j;
+            }
+        }
+        println!("{}", max_idx + 1);
+    }
+}
 
 /// A funtion to skip one line.
 fn skip_line() {
     let mut line = String::new();
     std::io::stdin().read_line(&mut line).unwrap();
-}
-
-/// A function that reads a single line of standard input and returns it.
-fn get_s() -> String {
-    let mut line = String::new();
-    std::io::stdin().read_line(&mut line).unwrap();
-    line.trim().to_string()
 }
 
 /// A function that reads a single line of standard input containing one value,
@@ -39,5 +55,3 @@ where
         .map(|n| n.parse::<T>().ok().unwrap())
         .collect::<Vec<T>>()
 }
-
-fn main() {}
